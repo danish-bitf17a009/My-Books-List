fc6249f44ada67965210d6d1eb075d585ec9e390
MUHAMMAD DANISH RIAZ <47732863+danish-bitf17a009@users.noreply.github.com>
2023-10-10 06:52:11 +0000

    Updated

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const router =express.Router();
+
+router.get('/books',);
+router.post('/users',);
+router.post('/addbook',);
+router.delete('/removebook',);
+router.put('/updatebook',);
+
+module.exports = router;
+
