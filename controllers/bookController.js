cd58732ab1c8dcae7b48050b3c7da4c414b9ea19
MUHAMMAD DANISH RIAZ <47732863+danish-bitf17a009@users.noreply.github.com>
2023-10-12 05:19:06 +0000

    Updated

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,5 +1,11 @@
+const Book = require('../models/book.js');
 const bookController = {
     async getBooks(req. res, next){
-            
+        try {
+            const book = await.book.find({});
+        } catch (error) {
+            return 
+        }
+        const book        
     }
 }
