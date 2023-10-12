0e42e694ce9d0260520264978796c6551670c9d4
MUHAMMAD DANISH RIAZ <47732863+danish-bitf17a009@users.noreply.github.com>
2023-10-12 06:05:28 +0000

    Updated

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,11 +1,34 @@
 const Book = require('../models/book.js');
+const Joi = require('joi');
+const {ValidationError} = require('joi');
 const bookController = {
-    async getBooks(req. res, next){
-        try {
-            const book = await.book.find({});
-        } catch (error) {
-            return 
+    async addBook(req, res, next){
+        const createBookSchema= Joi.object({
+            tittle: Joi.string().required,
+            author: Joi.string().required,
+            category: Joi.string().required,
+            link: Joi.string().required
+        });
+        const {error} = createBookSchema.validate(req.body);
+        if (error)
+        {
+            return next(error);
         }
-        const book        
-    }
-}
+        const {tittle, author, category, link} =req.body;
+        
+    },
+    async getBooks(req, res, next) {
+        let books;
+        try {
+            books = await Book.find({});
+
+        } catch (error) {
+            return next(error);
+        }
+        return res.status(200).json(books);
+    },
+
+};
+
+    
+
