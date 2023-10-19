5bf6cbf3c2b95e95a442c5a7afaa17eeaab15dc3
MUHAMMAD DANISH RIAZ <47732863+danish-bitf17a009@users.noreply.github.com>
2023-10-19 05:52:28 +0000

    Updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,21 @@
 
+const { response } = require('express');
+const User = require('../models/user.js');
+const Joi = require('joi');
+const {ValidationError} = require('joi');
+
 const userController = {
     async getUsers(req ,res ,next){
+        let users;
+        try {
+            users = await User.find({});
 
+        } catch (error) {
+            return next(error);
+        }
+        
+        return res.status(200).json(users);
     }
-}
+};
+
+module.exports = userController;
--- models/book.js
+++ models/book.js
@@ -5,7 +5,7 @@ const bookSchema = new Schema({
     tittle: { type: String, required: true },
     author: { type: String, required: true },
     category: { type: String, required: true},
-    links: { type: String, required: true }
+    links: { type: String }
 },
 {
      timestamp: true
